2bb2a3157710409d77777067216e189e1036b631
AshishAashu <ashishkaushalraj110@gmail.com>
2018-04-02 18:05:05 +0530

    Users modified.

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -7,12 +7,11 @@
     <h1 class="text-info text-center" >Registered Users</h1>
     <hr style="border-bottom: 2px solid #3d3d3d;width:50%;">
     <div class="table-responsive">
-        <table class="table" style="background: white; font-size: 1.5em;">
-            <thead class="">
-                <tr>
+        <table class="table" style="background: white; font-size: 1.6em;">
+            <thead >
+                <tr class="info">
                     <th>Id</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
+                    <th>Name</th>
                     <th>Email</th>
                     <th>Mobile</th>
                 </tr>
@@ -21,8 +20,7 @@
                 @foreach($users as $usr)
                     <tr>
                         <td>{{$usr->id}}</td>
-                        <td>{{$usr->firstname}}</td>
-                        <td>{{$usr->lastname}}</td>
+                        <td>{{$usr->firstname." ".$usr->lastname}}</td>
                         <td>{{$usr->useremail}}</td>
                         <td>{{$usr->usermob}}</td>
                     </tr>    
